e55ce7fbbfb4fa6bff976ba8d98f7ddaf9cd343d
folk.sriharat <sriharat.pro@ttspark.com>
2025-03-02 00:16:41 +0700

    test deploy

--- src/components/pages/dashboard/Dashboard.tsx
+++ src/components/pages/dashboard/Dashboard.tsx
@@ -2,7 +2,7 @@ import Layout from '@/components/modules/layouts/Layout';
 import React from 'react';
 
 const Dashboard = () => {
-  return <Layout>123</Layout>;
+  return <Layout>123123</Layout>;
 };
 
 export default Dashboard;
